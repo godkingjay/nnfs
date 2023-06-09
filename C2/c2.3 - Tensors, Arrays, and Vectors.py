feb45679a5fa0fd7889d031a08d4d644a20936e9
Jarrian <glorianagojar@gmail.com>
2023-06-09 15:16:53 +0800

    C2 - Tensors, Arrays, and Vectors (1/1)

---
+++ C2/c2.3 - Tensors, Arrays, and Vectors.py
@@ -0,0 +1,51 @@
+# List
+l = [1, 5, 6, 2]
+
+# List of Lists
+lol = [
+    [1, 5, 6, 2],
+    [3, 2, 1, 3]
+]
+
+# List of Lists of Lists
+lolol = [
+    [
+        [1, 5, 6, 2],
+        [3, 2, 1, 3]
+    ],
+    [
+        [5, 2, 1, 2],
+        [6, 4, 8, 4]
+    ],
+    [
+        [2, 8, 5, 3],
+        [1, 1, 9, 4]
+    ]
+]
+
+# 2D Matrix array
+# It has rows and columns
+# Shape is (3, 2) or 3x2, 3 rows and 2 columns
+list_of_matrix_array = [
+    [4, 2],
+    [5, 1],
+    [8, 2]
+]
+
+# 3D Matrix array
+# It has rows, columns, and depth
+# Shape is (3, 2, ) or 3x2x4, 3 depth, 2 rows, and 4 columns
+list_of_2d_matrix_array = [
+    [
+        [1, 5, 6, 2],
+        [3, 2, 1, 3]
+    ],
+    [
+        [5, 2, 1, 2],
+        [6, 4, 8, 4]
+    ],
+    [
+        [2, 8, 5, 3],
+        [1, 1, 9, 4]
+    ]
+]
