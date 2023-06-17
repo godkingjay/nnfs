1ae32affe71beafdf937ae076ddf11734ede951f
Jarrian <glorianagojar@gmail.com>
2023-06-17 08:54:45 +0800

    C5 - Practice 2 - ReLU

--- C5 - Calculating Network Loss with Loss/P2.py
+++ C5 - Calculating Network Loss with Loss/P2.py
@@ -6,17 +6,23 @@ from nnfs.datasets import spiral_data
 
 # Layer
 # Dense Class
+
+
 class Layer_Dense:
-  def __init__(self, n_inputs, n_neurons) -> None:
-    self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
-    self.biases = np.zeros((1, n_neurons))
-    
-  def forward(self, inputs):
-    self.outputs = np.dot(input, self.weights) + self.biases
+    def __init__(self, n_inputs, n_neurons) -> None:
+        self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+
+    def forward(self, inputs):
+        self.outputs = np.dot(input, self.weights) + self.biases
 
 # Activations
 # Rectified Linear Unit (ReLU) Class
 
+
+class Activation_ReLU:
+    def forward(self, inputs):
+        self.outputs = np.maximum(0, inputs)
 
 #  Softmax Activation Class
 
