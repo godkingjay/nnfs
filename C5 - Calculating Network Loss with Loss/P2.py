b10dd3cbd10a577e6fca60b266d41cb1b6cbff3a
Jarrian <glorianagojar@gmail.com>
2023-06-17 09:03:02 +0800

    C5 - Practice 2 - Categorical Cross Entropy Loss

--- C5 - Calculating Network Loss with Loss/P2.py
+++ C5 - Calculating Network Loss with Loss/P2.py
@@ -25,18 +25,45 @@ class Activation_ReLU:
         self.outputs = np.maximum(0, inputs)
 
 #  Softmax Activation Class
+
+
 class Activation_Softmax:
-  def forward(self, inputs):
-    exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
-    probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
-    self.outputs = probabilities
+    def forward(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.outputs = probabilities
 
 # Loss
 # Loss Class
 
 
+class Loss:
+    def calculate(self, outputs, y):
+        sample_losses = self.forward(outputs, y)
+        data_loss = np.mean(sample_losses)
+        return data_loss
+
 # Categorical Cross-Entropy Loss Class (Loss)
 
+
+class Loss_CategoricalCrossEntropy(Loss):
+    def forward(self, y_pred, y_true):
+        samples = len(y_pred)
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
+
+        if len(y_true.shape) == 1:
+            correct_confidences = y_pred_clipped[
+                range(samples),
+                y_true
+            ]
+        elif len(y_true.shape) == 2:
+            correct_confidences = np.sum(
+                y_pred_clipped * y_true,
+                axis=1
+            )
+
+        neg_log = -np.log(correct_confidences)
+        return neg_log
 
 # Accuracy Class
 
