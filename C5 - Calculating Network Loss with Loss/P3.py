3a685c8041d2e946e008bd3e7df7de5151529823
Jarrian <glorianagojar@gmail.com>
2023-06-18 10:00:38 +0800

    C5 - Practice 3 - Dense Class

--- C5 - Calculating Network Loss with Loss/P3.py
+++ C5 - Calculating Network Loss with Loss/P3.py
@@ -10,13 +10,13 @@ nnfs.init()
 # Dense Class
 
 
-# class Layer_Dense:
-#     def __init__(self, n_inputs, n_neurons) -> None:
-#         self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
-#         self.biases = np.zero((1, n_neurons))
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons) -> None:
+        self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zero((1, n_neurons))
 
-#     def forward(self, inputs):
-#         self.outputs = np.dot(inputs, self.weights) + self.biases
+    def forward(self, inputs):
+        self.outputs = np.dot(inputs, self.weights) + self.biases
 
 # Activations
 # Rectified Linear Unit (ReLU) Class
