d704262cc6aadd39aff6c93f9c36a3137a40c097
Jarrian <glorianagojar@gmail.com>
2023-06-18 10:00:25 +0800

    C5 - Practice 3 - Import Packages

---
+++ C5 - Calculating Network Loss with Loss/P3.py
@@ -0,0 +1,63 @@
+# Import Packages
+import numpy as np
+import nnfs
+
+from nnfs.datasets import spiral_data
+
+nnfs.init()
+
+# Layer
+# Dense Class
+
+
+# class Layer_Dense:
+#     def __init__(self, n_inputs, n_neurons) -> None:
+#         self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
+#         self.biases = np.zero((1, n_neurons))
+
+#     def forward(self, inputs):
+#         self.outputs = np.dot(inputs, self.weights) + self.biases
+
+# Activations
+# Rectified Linear Unit (ReLU) Class
+
+
+# class Activation_ReLU:
+#     def forward(self, inputs):
+#         self.outputs = np.maximum(0, inputs)
+
+# Softmax Class
+
+
+# class Activation_Softmax:
+#     def forward(self, inputs):
+#         exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+#         probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+#         self.outputs = probabilities
+
+# Loss
+# Loss Class
+
+
+# Categorical Cross-Entropy Class
+
+
+# Accuracy Class
+
+
+# Initialize Dataset
+
+
+# Implement Dense
+
+
+# Implement ReLU
+
+
+# Implement Softmax
+
+
+# Implement Loss
+
+
+# Implement Accuracy
