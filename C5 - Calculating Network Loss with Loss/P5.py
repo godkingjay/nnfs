517efc534fa590a53b72acb048d33872dab8ac66
Jarrian <glorianagojar@gmail.com>
2023-06-20 18:32:56 +0800

    C5 - Practice 5 - Loss Categorical Cross-Entropy Class

--- C5 - Calculating Network Loss with Loss/P5.py
+++ C5 - Calculating Network Loss with Loss/P5.py
@@ -38,8 +38,31 @@ class Activation_Softmax:
 # Loss Class
 
 
+class Loss:
+    def calculate(self, outputs, y):
+        pass
+
 # Categorical Cross-Entropy Class
 
+
+class Loss_CategoricalCrossEntropy:
+    def forward(sef, y_pred, y_true):
+        samples = len(y_pred)
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
+
+        if len(y_true.shape) == 1:
+            correct_confidences = y_pred_clipped[
+                range(samples),
+                y_true
+            ]
+        elif len(y_true.shape) == 2:
+            correct_confidences = np.sum(
+                y_pred * y_true,
+                axis=1
+            )
+
+        neg_log = -np.log(correct_confidences)
+        return neg_log
 
 # Accuracy Class
 
