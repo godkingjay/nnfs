e045676c37cc3dc0779688f881bd39c058168835
Jarrian <glorianagojar@gmail.com>
2023-06-24 14:26:07 +0800

    C5 - Practice 8 - Activation ReLU Class

--- C5 - Calculating Network Loss with Loss/P8.py
+++ C5 - Calculating Network Loss with Loss/P8.py
@@ -8,17 +8,23 @@ nnfs.init()
 
 # Layers
 # Dense Class
+
+
 class Layer_Dense:
-  def __init__(self, n_inputs, n_neurons) -> None:
-    self.weights = 0.01 * np.random.rand(n_inputs, n_neurons)
-    self.biases = np.zeros((1, n_neurons))
-  
-  def forward(self, inputs):
-    self.outputs = np.dot(inputs, self.weights) + self.biases
+    def __init__(self, n_inputs, n_neurons) -> None:
+        self.weights = 0.01 * np.random.rand(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+
+    def forward(self, inputs):
+        self.outputs = np.dot(inputs, self.weights) + self.biases
 
 # Activation
 # Rectified Linear Unit (ReLU) Class
 
+
+class Activation_ReLU:
+    def forward(self, inputs):
+        self.outputs = np.maximum(0, inputs)
 
 # Softmax Class
 
