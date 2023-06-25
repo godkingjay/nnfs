646db10543888dde0d8a5dba8e5a62c488b7b6f4
Jarrian <glorianagojar@gmail.com>
2023-06-25 10:04:32 +0800

    C5 - Practice 9 - Commented Sections

---
+++ C5 - Calculating Network Loss with Loss/P9.py
@@ -0,0 +1,43 @@
+# Import Libraries
+
+
+# Layers
+# Dense Class
+
+
+# Activation Functions
+# Rectified Linear Unit (ReLU) Activation Class
+
+
+# Softmax Activation Class
+
+
+# Loss Functions
+# Loss Class
+
+
+# Categorical Cross-Entropy Class
+
+
+# Accuracy Class
+
+
+# Initialize Dataset
+
+
+# Create 1st Hidden Layer
+
+
+# Implement Rectified Linear Unit (ReLU) Activation
+
+
+# Create 2nd Hidden Layer
+
+
+# Implement Softmax Activation
+
+
+# Calculate Loss
+
+
+# Calculate Accuracy
