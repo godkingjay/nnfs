9d0bc1fcb990598481599982b7f71e3dc74f96e1
Jarrian <glorianagojar@gmail.com>
2023-06-26 11:07:09 +0800

    C5 - Practice 10 - Softmax Activation Class

--- C5 - Calculating Network Loss with Loss/P10.py
+++ C5 - Calculating Network Loss with Loss/P10.py
@@ -29,6 +29,12 @@ class Activation_ReLU:
 # Softmax Activation Class
 
 
+class Activation_Softmax:
+    def forward(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.outputs = probabilities
+
 # Loss Function
 # Loss Class
 
