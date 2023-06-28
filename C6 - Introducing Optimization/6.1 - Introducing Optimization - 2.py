63b217b632e942a56ffc8fb83e993720fb6d1d03
Jarrian <glorianagojar@gmail.com>
2023-06-28 14:16:03 +0800

    C6 - Introducing Optimization (2/3)

---
+++ C6 - Introducing Optimization/6.1 - Introducing Optimization - 2.py
@@ -0,0 +1,104 @@
+import numpy as np
+import nnfs
+
+from nnfs.datasets import vertical_data
+
+nnfs.init()
+
+
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons) -> None:
+        self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+
+    def forward(self, inputs):
+        self.outputs = np.dot(inputs, self.weights) + self.biases
+
+
+class Activation_ReLU:
+    def forward(self, inputs):
+        self.outputs = np.maximum(0, inputs)
+
+
+class Activation_Softmax:
+    def forward(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.outputs = probabilities
+
+
+class Loss:
+    def calculate(self, outputs, y):
+        sample_losses = self.forward(outputs, y)
+        data_loss = np.mean(sample_losses)
+        return data_loss
+
+
+class Loss_CategoricalCrossEntropy(Loss):
+    def forward(self, y_pred, y_true):
+        samples = len(y_pred)
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
+
+        if len(y_true.shape) == 1:
+            correct_confidences = y_pred_clipped[
+                range(samples),
+                y_true
+            ]
+        elif len(y_true.shape) == 2:
+            correct_confidences = np.sum(
+                y_pred_clipped * y_true,
+                axis=1
+            )
+
+        neg_log = -np.log(correct_confidences)
+        return neg_log
+
+
+class Accuracy:
+    def calculate(self, outputs, y):
+        predictions = np.argmax(outputs, axis=1)
+
+        if len(y.shape) == 2:
+            y = np.argmax(y, axis=1)
+
+        accuracy = np.mean(predictions == y)
+        return accuracy
+
+
+X, y = vertical_data(samples=100, classes=3)
+
+dense1 = Layer_Dense(2, 3)
+activation1 = Activation_ReLU()
+dense2 = Layer_Dense(3, 3)
+activation2 = Activation_Softmax()
+loss1 = Loss_CategoricalCrossEntropy()
+accuracy1 = Accuracy()
+
+lowest_loss = 9999999
+best_dense1_weights = dense1.weights.copy()
+best_dense1_biases = dense1.biases.copy()
+best_dense2_weights = dense1.weights.copy()
+best_dense2_biases = dense1.biases.copy()
+
+for iteration in range(10000):
+    dense1.weights = 0.05 * np.random.randn(2, 3)
+    dense1.biases = 0.05 * np.random.randn(1, 3)
+    dense2.weights = 0.05 * np.random.randn(3, 3)
+    dense2.biases = 0.05 * np.random.randn(1, 3)
+
+    dense1.forward(X)
+    activation1.forward(dense1.outputs)
+    dense2.forward(activation1.outputs)
+    activation2.forward(dense2.outputs)
+
+    loss = loss1.calculate(activation2.outputs, y)
+    accuracy = accuracy1.calculate(activation2.outputs, y)
+
+    if loss < lowest_loss:
+        print(
+            f'New set of weights found, iteration: {iteration}, loss: {loss}, accuracy: {accuracy}')
+        best_dense1_weights = dense1.weights.copy()
+        best_dense1_biases = dense1.biases.copy()
+        best_dense2_weights = dense2.weights.copy()
+        best_dense2_biases = dense2.biases.copy()
+        lowest_loss = loss
