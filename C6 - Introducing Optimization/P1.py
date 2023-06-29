04f34243916da217b77005a756a1a93d0f12a66a
Jarrian <glorianagojar@gmail.com>
2023-06-29 10:52:52 +0800

    C6 - Practice 1 - Commented Sections

---
+++ C6 - Introducing Optimization/P1.py
@@ -0,0 +1,56 @@
+# Import Libraries
+
+
+# Layers
+# Dense Class
+
+
+# Activation Functions
+# Rectified Linear Unit (ReLu) Activation Class
+
+
+# Softmax Activation Class
+
+
+# Loss Functions
+# Loss Class
+
+
+# Categorical Cross-Entropy Class
+
+
+# Accuracy Class
+
+
+# Initialize Dataset
+
+
+# Create Layers, Activations, Loss Function, and Accuracy Objects
+
+
+# Initialize Helper Variables
+
+
+# Perform Iteration
+
+# Generate new set of weights and biases
+
+# Implement 1st Dense Layer Forward Pass
+
+# Implement Rectified Linear Activation Forward Pass
+
+# Implement 2nd Dense Layer Forward Pass
+
+# Implement Softmax Activation Forward Pass
+
+# Calculate Loss
+
+# Calculate Accuracy
+
+# Check if current loss is less than lowest loss
+# Print the iteration number, loss, and accuracy
+
+# Copy the current iteration's weights, biases, and loss to helper variables
+
+# Else
+# Copy best weights and biases to dense object properties
