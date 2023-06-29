2b5c31f76ff87e6a6d876c1d6c61055786ba5bd8
Jarrian <glorianagojar@gmail.com>
2023-06-29 16:38:46 +0800

    C6 - Practice 1 - Dense Layer Class

--- C6 - Introducing Optimization/P1.py
+++ C6 - Introducing Optimization/P1.py
@@ -10,6 +10,14 @@ nnfs.init()
 # Dense Class
 
 
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons) -> None:
+        self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+
+    def forward(self, inputs):
+        self.outputs = np.dot(inputs, self.weights) + self.biases
+
 # Activation Functions
 # Rectified Linear Unit (ReLu) Activation Class
 
