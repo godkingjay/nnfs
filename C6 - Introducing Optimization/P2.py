b894967832f7687ccfc5bb5d7fd73e9ba451f933
Jarrian <glorianagojar@gmail.com>
2023-06-30 11:12:30 +0800

    C6 - Practice 2 - Initialize Dataset

--- C6 - Introducing Optimization/P2.py
+++ C6 - Introducing Optimization/P2.py
@@ -80,8 +80,9 @@ class Accuracy:
         accuracy = np.mean(predictions == y)
         return accuracy
 
-# Initialize Dataset
 
+# Initialize Dataset
+X, y = vertical_data(samples=100, classes=3)
 
 # Create 1st and 2nd Hidden Layer
 
