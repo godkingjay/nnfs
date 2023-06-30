9b678e58a1b8317f2afb9f3e43d93ac511733f19
Jarrian <glorianagojar@gmail.com>
2023-06-30 10:10:15 +0800

    C6 - Practice 2 - Commented Sections

---
+++ C6 - Introducing Optimization/P2.py
@@ -0,0 +1,40 @@
+# Import Libraries
+
+
+# Layers
+# Dense Layer Class
+
+
+# Activation Functions
+# Rectified Linear Unit (ReLU) Activation Class
+
+
+# Softmax Activation Class
+
+
+# Loss Functions
+# Loss Class
+
+
+# Categorical Cross-Entropy Class
+
+
+# Accuracy Class
+
+
+# Initialize Dataset
+
+
+# Create 1st and 2nd Hidden Layer
+
+
+# Create Activation Functions objects
+
+
+# Create Loss and Accuracy Function objects
+
+
+# Initialize Helper Variables
+
+
+# Perform Iteration
