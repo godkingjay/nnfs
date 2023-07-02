c67ef01d9b0b3a278675fa33bfa7358a96702a7c
Jarrian <glorianagojar@gmail.com>
2023-07-02 10:10:47 +0800

    C7 - The Numerical Derivative (3/3)

---
+++ C7 - Derivatives/7.3 - The Numerical Derivative - 3.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def f(x):
+    return 2*x**2
+
+
+x = np.arange(0, 5, 0.001)
+y = f(x)
+
+plt.plot(x, y)
+
+colors = ['k', 'g', 'r', 'b', 'c']
+
+
+def approximate_tangent_line(x, approximate_derivative):
+    return (approximate_derivative * x) + b
+
+
+for i in range(5):
+    p2_delta = 0.0001
+    x1 = i
+    x2 = x1 + p2_delta
+
+    y1 = f(x1)
+    y2 = f(x2)
+
+    print((x1, y1), (x2, y2))
+    approximate_derivative = (y2 - y1) / (x2 - x1)
+    b = y2 - (approximate_derivative * x2)
+
+    xd = [x1 - 0.9, x1, x1 + 0.9]
+    yd = [approximate_tangent_line(
+        point, approximate_derivative) for point in xd]
+
+    plt.scatter(x1, y1, c=colors[i])
+    plt.plot(xd, yd, c=colors[i])
+
+    print('Approximate derivative for f(x)',
+          f'where x = {x1} is {approximate_derivative}')
+
+plt.show()
