910133aa23684d22bccbe1b02f699b3f8ae67e81
Jarrian <glorianagojar@gmail.com>
2023-07-03 09:22:54 +0800

    C7 - Practice 1 - Method for Expression

--- C7 - Derivatives/P1.py
+++ C7 - Derivatives/P1.py
@@ -1,11 +1,14 @@
 # Import Libraries
-
+import matplotlib.pyplot as plt
+import numpy as np
 
 # Method for Expression
 
 
-# Create X and Y coordinates
+def f(x):
+    return (5*x**5) + (4*x**3) - 5
 
+# Create X and Y coordinates
 
 # Plot X and Y
 
