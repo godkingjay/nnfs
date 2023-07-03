13f411529d2633401a48d7cd486fe5cc25c82903
Jarrian <glorianagojar@gmail.com>
2023-07-03 09:37:40 +0800

    C7 - Practice 2 - Commented Sections

---
+++ C7 - Derivatives/P2.py
@@ -0,0 +1,25 @@
+# Import Libraries
+
+
+# Method for Expression
+
+
+# Create x and y coordinates
+
+
+# Plot x and y coordinates
+
+
+# Colors
+
+
+# Method for Derivative
+
+
+# Method for y-intercept
+
+
+# Iterate Range
+
+
+# Show Plot
