7099d61a0def749d825c74107c61a1075f3a5ddf
Jarrian <glorianagojar@gmail.com>
2023-07-03 08:58:25 +0800

    C7 - Practice 1 - Commented Sections

---
+++ C7 - Derivatives/P1.py
@@ -0,0 +1,25 @@
+# Import Libraries
+
+
+# Method for Expression
+
+
+# Create X and Y coordinates
+
+
+# Plot X and Y
+
+
+# Colors
+
+
+# Method for Derivative
+
+
+# Method for Tangent Line
+
+
+# Iterate Range
+
+
+# Show Plot
