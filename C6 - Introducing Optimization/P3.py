408a7b6b9743b331403629c5ed42745fdca58046
Jarrian <glorianagojar@gmail.com>
2023-07-04 14:21:04 +0800

    C6 - Practice 6 - Commented Sections

---
+++ C6 - Introducing Optimization/P3.py
@@ -0,0 +1,37 @@
+# Import Libraries
+
+
+# Layers
+# Dense Layer Class
+
+
+# Activation Functions
+# Rectified Linear Unit (ReLU) Activation Class
+
+
+# Softmax Activation Class
+
+
+# Loss Functions
+# Loss Class
+
+
+# Categorical Cross-Entropy Loss Class
+
+
+# Accuracy Class
+
+
+# Initialize Dataset
+
+
+# Create Hidden Dense Layers
+
+
+# Create object for Activation Functions
+
+
+# Create object for Loss and Accuracy
+
+
+# Optimization
