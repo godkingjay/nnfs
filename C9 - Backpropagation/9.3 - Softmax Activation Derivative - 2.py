0e8d8ef33c942ad3e14c48ecbed98b18b101e4a7
Jarrian <glorianagojar@gmail.com>
2023-07-06 11:24:49 +0800

    C9 - Softmax Activation Derivative (2/2)

---
+++ C9 - Backpropagation/9.3 - Softmax Activation Derivative - 2.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+class Activation_Softmax:
+    def forward(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.outputs = probabilities
+
+    def backward(self, dvalues):
+        self.dinputs = np.empty_like(dvalues)
+
+        for index, (single_output, single_dvalues) in enumerate(zip(self.outputs, dvalues)):
+            single_output = single_output.reshape(-1, 1)
+            jacobian_matrix = np.diagflat(
+                single_output) - np.dot(single_output, single_output.T)
+            self.dinputs[index] = np.dot(jacobian_matrix, single_dvalues)
