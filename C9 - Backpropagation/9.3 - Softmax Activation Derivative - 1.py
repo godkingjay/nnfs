971e03b9c9baae5ba744f68263dc5defbec2172d
Jarrian <glorianagojar@gmail.com>
2023-07-06 11:24:43 +0800

    C9 - Softmax Activation Derivative (1/2)

---
+++ C9 - Backpropagation/9.3 - Softmax Activation Derivative - 1.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+softmax_output = np.array([0.7, 0.1, 0.2]).reshape(-1, 1)
+
+# print(softmax_output)
+
+# print(np.eye(softmax_output.shape[0]))
+
+# print(softmax_output * np.eye(softmax_output.shape[0]))
+
+# print(np.diagflat(softmax_output))
+
+# print(np.dot(softmax_output, softmax_output.T))
+
+print(np.diagflat(softmax_output) - np.dot(softmax_output, softmax_output.T))
