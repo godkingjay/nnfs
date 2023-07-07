8e9e0d9e4cb707a0c0c55f204d500dd2cf70d3d2
Jarrian <glorianagojar@gmail.com>
2023-07-07 19:53:42 +0800

    10.1 - Stochastic Gradient Descent (1/1)

---
+++ C10 - Optimizers/10.1 - Stochastic Gradient Descent - 1.py
@@ -0,0 +1,163 @@
+import numpy as np
+import nnfs
+
+from nnfs.datasets import spiral_data
+
+nnfs.init()
+
+
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons) -> None:
+        self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+
+    def forward(self, inputs):
+        self.inputs = inputs
+        self.outputs = np.dot(inputs, self.weights) + self.biases
+
+    def backward(self, dvalues):
+        self.dweights = np.dot(self.inputs.T, dvalues)
+        self.dbiases = np.sum(dvalues, axis=0, keepdims=True)
+        self.dinputs = np.dot(dvalues, self.weights.T)
+
+
+class Activation_ReLU:
+    def forward(self, inputs):
+        self.inputs = inputs
+        self.outputs = np.maximum(0, inputs)
+
+    def backward(self, dvalues):
+        self.dinputs = dvalues.copy()
+        self.dinputs[self.inputs <= 0] = 0
+
+
+class Activation_Softmax:
+    def forward(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.outputs = probabilities
+
+    def backward(self, dvalues):
+        self.dinputs = np.empty_like(dvalues)
+
+        for index, (single_outputs, single_dvalues) in enumerate(zip(self.outputs, dvalues)):
+            single_outputs = single_outputs.reshape(-1, 1)
+            jacobian_matrix = np.diagflat(
+                single_outputs) - np.dot(single_outputs, single_outputs.T)
+            self.dinputs[index] = np.dot(jacobian_matrix, single_dvalues)
+
+
+class Loss:
+    def calculate(self, outputs, y_true):
+        sample_losses = self.forward(outputs, y_true)
+        data_loss = np.mean(sample_losses)
+        return data_loss
+
+
+class Loss_CategoricalCrossEntropy(Loss):
+    def forward(self, y_pred, y_true):
+        samples = len(y_pred)
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
+
+        if len(y_true.shape) == 1:
+            correct_confidences = y_pred_clipped[
+                range(samples),
+                y_true
+            ]
+        elif len(y_true.shape) == 2:
+            correct_confidences = np.sum(
+                y_pred_clipped * y_true,
+                axis=1
+            )
+
+        neg_log = -np.log(correct_confidences)
+        return neg_log
+
+    def backward(self, dvalues, y_true):
+        samples = len(dvalues)
+        labels = len(dvalues[0])
+
+        if len(y_true.shape) == 1:
+            y_true = np.eye(labels)[y_true]
+
+        self.dinputs = -y_true / dvalues
+        self.dinputs = self.dinputs / samples
+
+
+class Activation_Softmax_Loss_CategoricalCrossEntropy():
+    def __init__(self) -> None:
+        self.activation = Activation_Softmax()
+        self.loss_function = Loss_CategoricalCrossEntropy()
+
+    def forward(self, inputs, y_true):
+        self.activation.forward(inputs)
+        self.outputs = self.activation.outputs
+        return self.loss_function.calculate(self.outputs, y_true)
+
+    def backward(self, dvalues, y_true):
+        samples = len(dvalues)
+
+        if len(y_true.shape) == 2:
+            y_true = np.argmax(y_true, axis=1)
+
+        self.dinputs = dvalues.copy()
+        self.dinputs[
+            range(samples),
+            y_true
+        ] -= 1
+        self.dinputs = self.dinputs / samples
+
+
+class Accuracy:
+    def calculate(self, outputs, y):
+        predictions = np.argmax(outputs, axis=1)
+
+        if len(y.shape) == 2:
+            y = np.argmax(y, axis=1)
+
+        accuracy = np.mean(predictions == y)
+        return accuracy
+
+
+class Optimizer_SGD:
+    def __init__(self, learning_rate=1.0) -> None:
+        self.learning_rate = learning_rate
+
+    def update_params(self, layer):
+        layer.weights += -self.learning_rate * layer.dweights
+        layer.biases += -self.learning_rate * layer.dbiases
+
+
+X, y = spiral_data(samples=100, classes=3)
+
+dense1 = Layer_Dense(2, 64)
+activation1 = Activation_ReLU()
+dense2 = Layer_Dense(64, 3)
+loss_activation = Activation_Softmax_Loss_CategoricalCrossEntropy()
+optimizer = Optimizer_SGD()
+accuracy_function = Accuracy()
+
+for epoch in range(10001):
+    dense1.forward(X)
+
+    activation1.forward(dense1.outputs)
+
+    dense2.forward(activation1.outputs)
+
+    loss = loss_activation.forward(dense2.outputs, y)
+    accuracy = accuracy_function.calculate(dense2.outputs, y)
+
+    if not epoch % 100:
+        print(
+            f"Epoch: {epoch},",
+            f"Accuracy: {accuracy:.3f},",
+            f"Loss: {loss:.3f}"
+        )
+
+    loss_activation.backward(loss_activation.outputs, y)
+    dense2.backward(loss_activation.dinputs)
+    activation1.backward(dense2.dinputs)
+    dense1.backward(activation1.dinputs)
+
+    optimizer.update_params(dense1)
+    optimizer.update_params(dense2)
