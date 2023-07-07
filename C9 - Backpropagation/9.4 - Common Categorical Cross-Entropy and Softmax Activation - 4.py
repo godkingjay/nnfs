fix: Activation Softmax Backward Pass

C9 - Backpropagation/9.4 - Common Categorical Cross-Entropy and Softmax Activation - 4.py
@@ -23,11 +23,12 @@ class Layer_Dense:
 
 class Activation_ReLU:
     def forward(self, inputs):
+        self.inputs = inputs
         self.outputs = np.maximum(0, inputs)
 
     def backward(self, dvalues):
         self.dinputs = dvalues.copy()
-        self.dinputs[self.dinputs <= 0] = 0
+        self.dinputs[self.inputs <= 0] = 0
 
 
 class Activation_Softmax: