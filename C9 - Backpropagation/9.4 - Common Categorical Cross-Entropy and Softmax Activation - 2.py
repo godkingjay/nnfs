81904b815db97c5a65d3de7058327d4cc7696418
Jarrian <glorianagojar@gmail.com>
2023-07-07 13:42:26 +0800

    C9 - Common Categorical Cross-Entropy and Softmax Activation (2/4)

---
+++ C9 - Backpropagation/9.4 - Common Categorical Cross-Entropy and Softmax Activation - 2.py
@@ -0,0 +1,103 @@
+import numpy as np
+import nnfs
+
+nnfs.init()
+
+
+class Activation_Softmax:
+    def forward(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.outputs = probabilities
+
+    def backward(self, dvalues):
+        self.dinputs = np.empty_like(dvalues)
+
+        for index, (single_outputs, single_dvalues) in enumerate(zip(self.outputs, dvalues)):
+            single_outputs = single_outputs.reshape(-1, 1)
+            jacobian_matrix = np.diagflat(
+                single_outputs) - np.dot(single_outputs, single_outputs.T)
+            self.dinputs[index] = np.dot(jacobian_matrix, single_dvalues)
+
+
+class Loss:
+    def calculate(self, outputs, y):
+        sample_losses = self.forward(outputs, y)
+        data_loss = np.mean(sample_losses)
+        return data_loss
+
+
+class Loss_CategoricalCrossEntropy(Loss):
+    def forward(self, y_pred, y_true):
+        samples = len(y_pred)
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
+
+        if len(y_true.shape) == 1:
+            correct_confidences = y_pred_clipped[
+                range(samples),
+                y_true
+            ]
+        elif len(y_true.shape) == 2:
+            correct_confidences = np.sum(
+                y_pred_clipped * y_true,
+                axis=1
+            )
+
+        neg_log = -np.log(correct_confidences)
+        return neg_log
+
+    def backward(self, dvalues, y_true):
+        samples = len(dvalues)
+        labels = len(dvalues[0])
+
+        if len(y_true.shape) == 1:
+            y_true = np.eye(labels)[y_true]
+
+        self.dinputs = -y_true/dvalues
+        self.dinputs = self.dinputs / samples
+
+
+class Activation_Softmax_Loss_CategoricalCrossEntropy():
+    def __init__(self) -> None:
+        self.activation = Activation_Softmax()
+        self.loss = Loss_CategoricalCrossEntropy()
+
+    def forward(self, inputs, y_true):
+        self.activation.forward(inputs)
+        self.outputs = self.activation.outputs
+        return self.loss.calculate(self.outputs, y_true)
+
+    def backward(self, dvalues, y_true):
+        samples = len(dvalues)
+
+        if len(y_true.shape) == 2:
+            y_true = np.argmax(y_true, axis=1)
+
+        self.dinputs = dvalues.copy()
+        self.dinputs[range(samples), y_true] -= 1
+        self.dinputs = self.dinputs / samples
+
+
+softmax_outputs = np.array([[0.7, 0.1, 0.2],
+                            [0.1, 0.5, 0.4],
+                            [0.02, 0.9, 0.08]])
+
+class_targets = np.array([0, 1, 1])
+
+softmax_loss = Activation_Softmax_Loss_CategoricalCrossEntropy()
+softmax_loss.backward(softmax_outputs, class_targets)
+dvalues1 = softmax_loss.dinputs
+
+activation = Activation_Softmax()
+activation.outputs = softmax_outputs
+
+loss = Loss_CategoricalCrossEntropy()
+loss.backward(softmax_outputs, class_targets)
+
+activation.backward(loss.dinputs)
+dvalues2 = activation.dinputs
+
+print('Gradients: combined loss and activation:')
+print(dvalues1)
+print('Gradients: separate loss and activation:')
+print(dvalues2)
