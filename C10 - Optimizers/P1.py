7bd986aef0d6fce3af0ed7a96e9fd5bf5063cb7b
Jarrian <glorianagojar@gmail.com>
2023-07-08 12:22:38 +0800

    10 - Practice 1 - Activation Softmax Class Forward Pass

--- C10 - Optimizers/P1.py
+++ C10 - Optimizers/P1.py
@@ -40,6 +40,13 @@ class Activation_ReLU:
 # Softmax Activation Class
 
 
+class Activation_Softmax:
+    def forward(self, inputs):
+        self.inputs = inputs
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.outputs = probabilities
+
 # Loss
 # Loss Class
 
