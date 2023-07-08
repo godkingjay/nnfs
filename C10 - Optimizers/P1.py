556c4eeab4793322eb473464ede6ceceec396667
Jarrian <glorianagojar@gmail.com>
2023-07-08 12:26:45 +0800

    10 - Practice 1 - Loss Class

--- C10 - Optimizers/P1.py
+++ C10 - Optimizers/P1.py
@@ -60,6 +60,12 @@ class Activation_Softmax:
 # Loss Class
 
 
+class Loss:
+    def calculate(self, y_pred, y_true):
+        samples_losses = self.forward(y_pred, y_true)
+        data_loss = np.mean(samples_losses)
+        return data_loss
+
 # Categorical Cross-Entropy Loss Class
 
 
