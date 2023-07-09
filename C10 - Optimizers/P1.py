cf541aa63ec30d122a600c6a7c3eb981818732b2
Jarrian <glorianagojar@gmail.com>
2023-07-09 12:41:02 +0800

    10 - Practice 1 - Implement 2nd Dense HIdden Layer Forward Pass

--- C10 - Optimizers/P1.py
+++ C10 - Optimizers/P1.py
@@ -171,6 +171,7 @@ for epoch in range(10001):
     activation1.forward(dense1.outputs)
 
     # Implement 2nd Dense Layer Forward Pass
+    dense2.forward(activation1.outputs)
 
     # Implement Categorical Cross-Entropy Loss and Softmax Activation Forward Pass
 
