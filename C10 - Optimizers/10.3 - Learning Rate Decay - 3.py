19e0afc5c4ca4793fa1ddbd7d4bf6245b28aaaa4
Jarrian <glorianagojar@gmail.com>
2023-07-10 09:15:57 +0800

    10 - Learning Rate Decay (3/5)

---
+++ C10 - Optimizers/10.3 - Learning Rate Decay - 3.py
@@ -0,0 +1,7 @@
+starting_learning_rate = 1.0
+learning_rate_decay = 0.1
+
+for step in range(20):
+    learning_rate = starting_learning_rate * \
+        (1 / (1 + (learning_rate_decay * step)))
+    print(learning_rate)
