946ac2cd069db34d66dfdffc9d96eeeb23f2a911
Jarrian <glorianagojar@gmail.com>
2023-07-11 11:34:45 +0800

    C10 - Practice 2 - Import Libraries

---
+++ C10 - Optimizers/P2.py
@@ -0,0 +1,51 @@
+# Import Libraries
+
+
+# Layers
+# Dense Layer Class
+
+
+# Activation Functions
+# Rectified Linear Unit (ReLU) Activation Class
+
+
+# Softmax Activation Class
+
+
+# Loss
+# Loss Class
+
+
+# Categorical Cross-Entropy Loss Class
+
+
+# Loss and Activation Functions
+# Categorical Cross-Entropy Loss and Softmax Activation Class
+
+
+# Optimizers
+# Stochastic Gradient Descent (SGD) Optimizer Class
+
+
+# Accuracy Class
+
+
+# Initialize Dataset
+
+
+# Create 1st Dense Hidden Layer
+
+
+# Create 2nd Dense Hidden Layer
+
+
+# Create ReLU Activation Object
+
+
+# Create Softmax Classifier's combined Loss and Activation
+
+
+# Create Loss and Accuracy Object
+
+
+# Epoch Iteration
