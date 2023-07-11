088ac195ed2cf6e4fe64b4735ef0b4a8c6db255a
Jarrian <glorianagojar@gmail.com>
2023-07-11 12:12:45 +0800

    C10 - Practice 2 - Dense Layer Class Forward Pass

--- C10 - Optimizers/P2.py
+++ C10 - Optimizers/P2.py
@@ -15,6 +15,10 @@ class Layer_Dense:
         self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
         self.biases = np.zeros((1, n_neurons))
 
+    def forward(self, inputs):
+        self.inputs = inputs
+        self.outputs = np.dot(inputs, self.weights) + self.biases
+
 # Activation Functions
 # Rectified Linear Unit (ReLU) Activation Class
 
