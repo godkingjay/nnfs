31acc5f268692b0d6c9823d1ad371fdc47cfb5e8
Jarrian <glorianagojar@gmail.com>
2023-07-11 12:11:01 +0800

    C10 - Practice 2 - Dense Layer Class Initialization

--- C10 - Optimizers/P2.py
+++ C10 - Optimizers/P2.py
@@ -10,6 +10,11 @@ nnfs.init()
 # Dense Layer Class
 
 
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons) -> None:
+        self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+
 # Activation Functions
 # Rectified Linear Unit (ReLU) Activation Class
 
