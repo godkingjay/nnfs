b055ac238a7a6037ee617871d41877e09ff3b294
Jarrian <glorianagojar@gmail.com>
2023-07-12 08:23:10 +0800

    C10 - Practice 2 - Softmax Activation Class Forward Pass

--- C10 - Optimizers/P2.py
+++ C10 - Optimizers/P2.py
@@ -26,17 +26,26 @@ class Layer_Dense:
 
 # Activation Functions
 # Rectified Linear Unit (ReLU) Activation Class
+
+
 class Activation_ReLU:
     def forward(self, inputs):
         self.inputs = inputs
         self.outputs = np.maximum(0, inputs)
-        
+
     def backward(self, dvalues):
         self.dinputs = dvalues.copy()
         self.dinputs[self.inputs <= 0] = 0
 
 # Softmax Activation Class
 
+
+class Activation_Softmax:
+    def forward(self, inputs):
+        self.inputs = inputs
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.outputs = probabilities
 
 # Loss
 # Loss Class
