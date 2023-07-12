0693e2a93b45a7176ec28e57cbbbfa6664b065c8
Jarrian <glorianagojar@gmail.com>
2023-07-12 08:04:30 +0800

    C10 - Practice 2 - ReLU Activation Class Backward Pass

--- C10 - Optimizers/P2.py
+++ C10 - Optimizers/P2.py
@@ -30,6 +30,10 @@ class Activation_ReLU:
     def forward(self, inputs):
         self.inputs = inputs
         self.outputs = np.maximum(0, inputs)
+        
+    def backward(self, dvalues):
+        self.dinputs = dvalues.copy()
+        self.dinputs[self.inputs <= 0] = 0
 
 # Softmax Activation Class
 
