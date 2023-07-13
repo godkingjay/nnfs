25a8758ce9dd90d09c083531c70908675bc8e707
Jarrian <glorianagojar@gmail.com>
2023-07-13 08:09:36 +0800

    C10 - Practice 2 - SGD Optimizer Update Params

--- C10 - Optimizers/P2.py
+++ C10 - Optimizers/P2.py
@@ -141,6 +141,24 @@ class Optimizer_SGD:
         if self.decay:
             self.current_learning_rate = self.learning_rate * \
                 (1. / (1. + self.decay * self.iterations))
+    
+    def update_params(self, layer: Layer_Dense):
+        if self.momentum:
+            if not hasattr(layer, 'weight_momentums'):
+                layer.weight_momentums = np.zeros_like(layer.weights)
+                layer.bias_momentums = np.zeros_like(layer.biases)
+            
+            weight_updates = (self.momentum * layer.weight_momentums) - (self.current_learning_rate * layer.dweights)
+            layer.weight_momentums = weight_updates
+            
+            bias_updates = (self.momentum * layer.bias_momentums) - (self.current_learning_rate * layer.dbiases)
+            layer.bias_momentums = bias_updates
+        else:
+            weight_updates = -self.current_learning_rate * layer.dweights
+            bias_updates = -self.current_learning_rate * layer.dbiases
+        
+        layer.weights += weight_updates
+        layer.biases += bias_updates
 
     def post_update_params(self):
         self.iterations += 1
