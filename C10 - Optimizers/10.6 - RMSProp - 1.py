9b93a130eeb0bdce84c20b1ccf1b8259cd70e100
Jarrian <glorianagojar@gmail.com>
2023-07-15 15:21:31 +0800

    C10 - Root Mean Square Propagation (RMSProp) Optimizer (1/1)

--- C10 - Optimizers/10.6 - RMSProp - 1.py
+++ C10 - Optimizers/10.6 - RMSProp - 1.py
@@ -214,8 +214,19 @@ class Optimizer_RMSProp:
             self.current_learning_rate = self.learning_rate * \
                 (1. / (1. + self.decay * self.iterations))
 
-    def update_params():
-        pass
+    def update_params(self, layer: Layer_Dense):
+        if not hasattr(layer, "weight_cache"):
+            layer.weight_cache = np.zeros_like(layer.weights)
+            layer.bias_cache = np.zeros_like(layer.biases)
+
+        layer.weight_cache = self.rho * layer.weight_cache + \
+            (1 - self.rho) * layer.dweights**2
+        layer.bias_cache = self.rho * layer.bias_cache + \
+            (1 - self.rho) * layer.dbiases**2
+        layer.weights += -self.current_learning_rate * \
+            layer.dweights / (np.sqrt(layer.weight_cache) + self.epsilon)
+        layer.biases += -self.current_learning_rate * \
+            layer.dbiases / (np.sqrt(layer.bias_cache) + self.epsilon)
 
     def post_update_params(self):
         self.iterations += 1
@@ -250,7 +261,7 @@ activation1 = Activation_ReLU()
 loss_activation = Loss_CategoricalCrossEntropy_Activation_Softmax()
 
 # Create SGD Optimizer
-optimizer_sgd = Optimizer_AdaGrad(decay=1e-4)
+optimizer_sgd = Optimizer_RMSProp(decay=1e-4)
 
 # Create Accuracy Object
 accuracy_function = Accuracy()
