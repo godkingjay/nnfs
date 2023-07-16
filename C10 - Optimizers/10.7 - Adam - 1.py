8dc1a8fc70864353f53f885b81cad18812e3cf90
Jarrian <glorianagojar@gmail.com>
2023-07-16 09:22:24 +0800

    C10 - Adaptive Momentum (Adam) - 1.2

--- C10 - Optimizers/10.7 - Adam - 1.py
+++ C10 - Optimizers/10.7 - Adam - 1.py
@@ -238,8 +238,21 @@ class Optimizer_RMSProp:
 
 
 class Optimizer_Adam:
-    def __init__(self) -> None:
-        pass
+    def __init__(
+        self,
+        learning_rate=0.001,
+        decay=0.,
+        epsilon=1e-7,
+        beta_1=0.9,
+        beta_2=0.999
+    ) -> None:
+        self.learning_rate = learning_rate
+        self.current_learning_rate = learning_rate
+        self.decay = decay
+        self.iterations = 0
+        self.epsilon = epsilon
+        self.beta_1 = beta_1
+        self.beta_2 = beta_2
 
     def pre_update_params(self):
         pass
